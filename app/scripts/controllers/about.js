f1459aa8336382827077349026d11376abadd2ce
Alexander <alex.narkevich.85@gmail.com>
2015-10-14 00:59:12 +0300

    first upload

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name epamTestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the epamTestApp
+ */
+angular.module('epamTestApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
